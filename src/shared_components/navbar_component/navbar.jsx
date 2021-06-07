8c011ed9e57be7b828cdc1d1a68906b5b535f7ca
hussien kamal <hussien.kamal92@gmail.com>
2021-06-07 12:09:35 +0200

    add libraries and structure for the project and start in navbar

---
+++ src/shared_components/navbar_component/navbar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import "./navbar_style.scss"
+
+class Navbar extends Component {
+    state = {}
+    render() {
+        return (
+            <h1>lol</h1>
+        );
+    }
+}
+
+export default Navbar;
