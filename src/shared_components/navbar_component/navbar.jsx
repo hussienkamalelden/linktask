42f838a2ceaef33914414372d9dfd82e0aa17bc6
hussien kamal <hussien.kamal92@gmail.com>
2021-06-08 19:42:11 +0200

    finished first phase in navbar

--- src/shared_components/navbar_component/navbar.jsx
+++ src/shared_components/navbar_component/navbar.jsx
@@ -4,19 +4,41 @@ import logo from "../../assets/images/logo.png"
 
 class Navbar extends Component {
     state = {}
+    // onclick Menu Toggle ..
+    menuToggle = () => {
+        let menu = document.querySelector('.menu-icon')
+        let dropdown = document.querySelector('.dropdown-slide')
+        menu.classList.toggle('open');
+        dropdown.classList.toggle('dropdown-tog');
+    }
     render() {
         return (
             <React.Fragment>
-                <article className="g-navbar">
-                    <div className="holder">
-                        <nav className="navbar sticky-top navbar-expand-lg">
-                            <div className="container">
-                                <a className="navbar-brand" href="#">
-                                    <img src={logo} alt="link development logo" />
-                                </a>
+                <article className="nav-holder">
+                    <section className="navbar fixed-top">
+                        <div className="container">
+                            <div className="holder">
+                                <section className="nav-left">
+                                    <div className="menu-icon" onClick={this.menuToggle}>
+                                        <span></span>
+                                        <span></span>
+                                        <span></span>
+                                    </div>
+                                    <a className="main-logo" href="#">
+                                        <img src={logo} alt="link development logo" />
+                                    </a>
+                                </section>
+                                <section className="nav-right">
+                                    <a className="main-logo" href="#">
+                                        <img src={logo} alt="link development logo" />
+                                    </a>
+                                </section>
                             </div>
-                        </nav>
-                    </div>
+                            <section className="dropdown-slide">
+
+                            </section>
+                        </div>
+                    </section>
                 </article>
             </React.Fragment>
         );
