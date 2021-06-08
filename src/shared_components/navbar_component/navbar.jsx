cfc55a734b4adad5507f4b8a72a1e84f97b1efd4
hussien kamal <hussien.kamal92@gmail.com>
2021-06-08 21:31:15 +0200

    finished second phase in navbar (menu - dropdown menu - dropdown search)

--- src/shared_components/navbar_component/navbar.jsx
+++ src/shared_components/navbar_component/navbar.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import "./navbar_style.scss"
 import logo from "../../assets/images/logo.png"
+import { Link } from 'react-router-dom';
 
 class Navbar extends Component {
     state = {}
@@ -35,7 +36,55 @@ class Navbar extends Component {
                                 </section>
                             </div>
                             <section className="dropdown-slide">
-
+                                <div className="dropdown-left">
+                                    <div className="dropdown-media">
+                                        <a className="social-links" href="#"><i className="icons fab fa-twitter"></i></a>
+                                        <a className="social-links" href="#"><i className="icons fab fa-facebook-f"></i></a>
+                                        <a className="social-links" href="#"><i className="icons fab fa-linkedin-in"></i></a>
+                                        <a className="social-links" href="#"><i className="icons fab fa-youtube"></i></a>
+                                        <a className="social-links" href="#"><i className="icons fab fa-instagram"></i></a>
+                                    </div>
+                                </div>
+                                <div className="dropdown-right">
+                                    {/* Start Top Section */}
+                                    <div className="dropdown-right-top">
+                                        <ul className="nav-dropdown-top-ul">
+                                            <li className="nav-dropdown-top-li"><a className="nav-dropdown-main-links nav-dropdown-links-active" href="#">Home</a></li>
+                                            <li className="nav-dropdown-top-li"><a className="nav-dropdown-main-links" href="#">About us</a>
+                                                <ul className="nav-dropdown-top-sub-ul">
+                                                    <li className="nav-dropdown-top-sub-li"><a href="#" className="nav-dropdown-main-sub-links">who are us</a></li>
+                                                    <li className="nav-dropdown-top-sub-li"><a href="#" className="nav-dropdown-main-sub-links">why us?</a></li>
+                                                </ul>
+                                            </li>
+                                            <li className="nav-dropdown-top-li"><a className="nav-dropdown-main-links" href="#">News</a>
+                                                <ul className="nav-dropdown-top-sub-ul">
+                                                    <li className="nav-dropdown-top-sub-li"><a href="#" className="nav-dropdown-main-sub-links">News</a></li>
+                                                    <li className="nav-dropdown-top-sub-li"><a href="#" className="nav-dropdown-main-sub-links">Events</a></li>
+                                                </ul>
+                                            </li>
+                                            <li className="nav-dropdown-top-li"><a className="nav-dropdown-main-links" href="#">Careers</a>
+                                                <ul className="nav-dropdown-top-sub-ul">
+                                                    <li className="nav-dropdown-top-sub-li"><a href="#" className="nav-dropdown-main-sub-links">Opportunities</a></li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    {/* End Top Section */}
+                                    {/* Start Search Section */}
+                                    <form className="search-box">
+                                        <input className="search inputs" type="search" name="search" placeholder="Search" />
+                                        <input className="submit inputs" type="submit" value="" />
+                                    </form>
+                                    {/* End Search Section */}
+                                    {/* Start Bottom Section */}
+                                    <div className="dropdown-right-bottom">
+                                        <ul className="nav-dropdown-bottom-ul">
+                                            <li className="nav-dropdown-bottom-li"><a className="nav-dropdown-main-links" href="#">Contact Us</a></li>
+                                            <li className="nav-dropdown-bottom-li"><a className="nav-dropdown-main-links" href="#">Site Map</a></li>
+                                        </ul>
+                                    </div>
+                                    {/* End Bottom Section */}
+                                </div>
                             </section>
                         </div>
                     </section>
