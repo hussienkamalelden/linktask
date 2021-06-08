87c78f0570eb58ad0b9ded703e90b81239bdf5ff
hussien kamal <hussien.kamal92@gmail.com>
2021-06-08 12:57:56 +0200

    finished footer and continue in navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from './shared_components/navbar_component/navbar';
+import Footer from './shared_components/footer_component/footer';
 
 class App extends Component {
   state = {}
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <React.Fragment>
         <Navbar />
+        <Footer />
       </React.Fragment>
     );
   }
--- src/shared_components/navbar_component/navbar.jsx
+++ src/shared_components/navbar_component/navbar.jsx
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import "./navbar_style.scss"
+import logo from "../../assets/images/logo.png"
 
 class Navbar extends Component {
     state = {}
     render() {
         return (
-            <h1>lol</h1>
+            <React.Fragment>
+                <article className="g-navbar">
+                    <div className="holder">
+                        <nav className="navbar sticky-top navbar-expand-lg">
+                            <div className="container">
+                                <a className="navbar-brand" href="#">
+                                    <img src={logo} alt="link development logo" />
+                                </a>
+                            </div>
+                        </nav>
+                    </div>
+                </article>
+            </React.Fragment>
         );
     }
 }
