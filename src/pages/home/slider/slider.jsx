578a98948235d1f6d2cd9d16fc3080203703568e
hussien kamal <hussien.kamal92@gmail.com>
2021-06-10 16:13:16 +0200

    finished Home Slider component and start in How component

--- src/pages/home/slider/slider.jsx
+++ src/pages/home/slider/slider.jsx
@@ -1,11 +1,47 @@
 import React, { Component } from 'react';
+import { Carousel } from "react-bootstrap"
 import "./slider_style.scss"
 
 class Slider extends Component {
     state = {}
     render() {
         return (
-            <h1>Slider</h1>
+            <React.Fragment>
+                <article className="slider">
+                    <Carousel className="carousel" fade>
+                        <Carousel.Item className="carousel-item">
+                            <div className="slider-slide-holder">
+                                <div className="slider-slide-holder-overlay"></div>
+                            </div>
+                            <Carousel.Caption className="slider-slide-caption">
+                                <h1 className="slider-slide-caption-title">AI Foundation</h1>
+                                <p className="slider-slide-caption-content">is focused on inspiring the next generation of kids Healthcare professionals.</p>
+                                <a className="slider-slide-caption-link" href="#">Donate Now</a>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <div className="slider-slide-holder">
+                                <div className="slider-slide-holder-overlay"></div>
+                            </div>
+                            <Carousel.Caption className="slider-slide-caption">
+                                <h1 className="slider-slide-caption-title">AI Foundation</h1>
+                                <p className="slider-slide-caption-content">is focused on inspiring the next generation of kids Healthcare professionals.</p>
+                                <a className="slider-slide-caption-link" href="#">Donate Now</a>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <div className="slider-slide-holder">
+                                <div className="slider-slide-holder-overlay"></div>
+                            </div>
+                            <Carousel.Caption className="slider-slide-caption">
+                                <h1 className="slider-slide-caption-title">AI Foundation</h1>
+                                <p className="slider-slide-caption-content">is focused on inspiring the next generation of kids Healthcare professionals.</p>
+                                <a className="slider-slide-caption-link" href="#">Donate Now</a>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    </Carousel>
+                </article>
+            </React.Fragment>
         );
     }
 }
