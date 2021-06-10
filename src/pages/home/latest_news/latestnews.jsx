de1171ffd2b2255679b7f500af88bcda339e6e85
hussien kamal <hussien.kamal92@gmail.com>
2021-06-11 01:50:47 +0200

    start in shared component 'news_card' component

--- src/pages/home/latest_news/latestnews.jsx
+++ src/pages/home/latest_news/latestnews.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import "./latestnews_style.scss"
+import NewsCard from '../../../shared_components/news_card_component/news_card';
 
 class LatestNews extends Component {
     state = {}
     render() {
         return (
-            <h1>LatestNews</h1>
+            <React.Fragment>
+                <h1>LatestNews</h1>
+                <NewsCard />
+            </React.Fragment>
         );
     }
 }
---
+++ src/shared_components/news_card_component/news_card.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import "./news_card_style.scss"
+
+class NewsCard extends Component {
+    state = {}
+    render() {
+        return (
+            <React.Fragment>
+                <article className="news-card">
+                    hello
+                </article>
+            </React.Fragment>
+        );
+    }
+}
+
+export default NewsCard;
