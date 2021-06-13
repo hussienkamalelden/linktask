7ea9b796ecf0848e221b38aba883a8354904c43a
hussien kamal <hussien.kamal92@gmail.com>
2021-06-13 02:43:30 +0200

    finished related news component in 'news details' page

--- src/pages/news_details/related_topics/related_topics.jsx
+++ src/pages/news_details/related_topics/related_topics.jsx
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import "./related_topics_style.scss";
+import RelatedCard from './../../../shared_components/related_card_component/related_card';
 
 class RelatedTopics extends Component {
     state = {}
     render() {
         return (
             <React.Fragment>
-                <h1>RelatedTopics</h1>
+                <article className="related-topics">
+                    <div className="container">
+                        <h2>Related Topics</h2>
+                        <div className="row">
+                            <RelatedCard />
+                            <RelatedCard />
+                            <RelatedCard />
+                        </div>
+                    </div>
+                </article>
             </React.Fragment>
         );
     }
